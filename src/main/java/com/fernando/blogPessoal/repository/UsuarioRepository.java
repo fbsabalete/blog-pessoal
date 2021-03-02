7385b8c552e609b010b793bd5ca6570005d1fb9c
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-02 11:15:47 -0300

    refact(UsuarioRepository): Modificado o método de busca

--- src/main/java/com/fernando/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/com/fernando/blogPessoal/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    public Optional<Usuario> findByUsuarioContainingIgnoreCase(Usuario usuario);
+    public Optional<Usuario> findByUsuario(String usuario);
 }
