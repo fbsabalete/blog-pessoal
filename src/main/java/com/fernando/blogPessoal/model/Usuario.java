80548a3b76c8e230672ab5d4007c01fb48bc9cc2
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-02 10:53:58 -0300

    Criadas models de usuario

---
+++ src/main/java/com/fernando/blogPessoal/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.fernando.blogPessoal.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nome;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String usuario;
+
+    @NotNull
+    @Size(min = 6, max = 50)
+    private String senha;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
