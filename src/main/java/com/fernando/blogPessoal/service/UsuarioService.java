d1fbf5feca56e96a5bb611af4fb975524a97b481
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-02 11:28:25 -0300

    feat(security): Criado UsuarioService

---
+++ src/main/java/com/fernando/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,48 @@
+package com.fernando.blogPessoal.service;
+
+import com.fernando.blogPessoal.model.UserLogin;
+import com.fernando.blogPessoal.model.Usuario;
+import com.fernando.blogPessoal.repository.UsuarioRepository;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    public Usuario cadastrarUsuario(Usuario usuario){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String senhaEncoder = encoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaEncoder);
+
+        return repository.save(usuario);
+    }
+
+    public Optional<UserLogin> logar(Optional<UserLogin> user){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Optional<Usuario> usuario = repository.findByUsuario(user.get().getEmail());
+
+        if(usuario.isPresent()){
+            if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
+                String auth = user.get().getEmail() + ":" + user.get().getSenha();
+                byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encodedAuth);
+
+                user.get().setToken(authHeader);
+                user.get().setEmail(usuario.get().getNome());
+
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
